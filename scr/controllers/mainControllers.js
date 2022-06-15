ef071dbdb3962b2905cf52a259df4e14bd3d1514
PeterSauma <pedrosaumacammi@gmail.com>
2022-06-14 22:58:06 -0300

    index funcionando y con estilo, arreglar carrusel

--- scr/controllers/mainControllers.js
+++
@@ -1,9 +0,0 @@
-const controller ={
-    index: (req, res) => {
-        res.render('index')},
-    
-
-}
-
-
-module.exports = controller
---
+++ src/controllers/mainControllers.js
@@ -0,0 +1,9 @@
+const controller ={
+    index: (req, res) => {
+        res.render('index')},
+    
+
+}
+
+
+module.exports = controller
