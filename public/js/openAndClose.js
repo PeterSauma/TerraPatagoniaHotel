495979a8393ea9d1efdc294be98ff998c346c453
Peter Sauma <petersauma@gmail.com>
2024-05-11 09:39:32 -0300

    se arregla carrusel de spa

--- public/js/openAndClose.js
+++ public/js/openAndClose.js
@@ -3,19 +3,19 @@ window.addEventListener('load', function(){
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>queySelectors<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
     //Captura de boton 'abrir'. boton 'cerrar' y carrusel
-    let fullCarrusell = document.querySelector('#fullCarrusell')
+    let fullCarrusel = document.querySelector('#fullCarrusel')
     let openCarrusel =document.querySelector('#openCarrusel')
     let closeCarrusel =document.querySelector('#closeCarrusel')
   
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Evento abrir carrusel<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     openCarrusel.addEventListener('click', function(){
-      fullCarrusell.style.display= 'flex'; 
+      fullCarrusel.style.display= 'flex'; 
     })
   
    
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Evento cerrar carrusel<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     closeCarrusel.addEventListener('click', function(){
-      fullCarrusell.style.display= 'none'; 
+      fullCarrusel.style.display= 'none'; 
     })
            
    })
